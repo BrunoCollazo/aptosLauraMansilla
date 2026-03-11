db9d4e480eafd5707a4e787462a657fab7fd8219
BrunoCollazo <collazobruno2@gmail.com>
2026-03-11 14:15:27 -0300

    FEAT: Added springboot base

---
+++ laura-apartments-backend/src/main/java/com/lauramansilla/laura_apartments_backend/LauraApartmentsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lauramansilla.laura_apartments_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LauraApartmentsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LauraApartmentsBackendApplication.class, args);
+	}
+
+}
